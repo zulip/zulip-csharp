fdcb76a3f5f95588d0116ae1e7115a9abc1af7df
cPhost <priyank3219@gmail.com>
2017-12-13 15:58:59 +0000

    ci testing: set up travis ci for testing

--- TestServer/index.js
+++ TestServer/index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const util = require('util')
 const express = require('express')
+const EventEmitter = require('events')
 const bodyParser = require('body-parser')
 const { authParser } = require('./helpers')
 
@@ -28,7 +29,17 @@ app.post('/test', (request, response) => {
   `);
 })
 
+class Manager extends EventEmitter {}
+const manager = new Manager()
+
 const port = +process.env.PORT || 9991
-app.listen(port, () => {
+var server = app.listen(port, () => {
   console.log(`Testing Server started at ${port}`)
-});
+  startTests()
+})
+
+function startTests() {
+  manager.emit('server-started', server)
+}
+
+module.exports = manager
---
+++ run-tests.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+cd TestSever
+node TestServer
