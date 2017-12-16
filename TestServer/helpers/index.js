32eb6f753ccd8bce5c310f3f4a08a24390f2fcfe
cPhost <priyank3219@gmail.com>
2017-12-15 20:35:37 -0500

    test server: improve test server demo page

--- TestServer/helpers/index.js
+++ TestServer/helpers/index.js
@@ -1,28 +1,27 @@
-const util = require('util')
-const path = require('path')
-const fs = require('fs')
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
 
-const data = require('./data')
-const { errorJSON } = data
+const data = require('./data');
+const { errorJSON } = data;
 
 function formatJSON(json) {
-  return util.format("%o", json)
+  return util.format("%o", json);
 }
 
 exports.authParser = function(auth) {
   if(!auth) {
     let error = {
       err: formatJSON(errorJSON)
-    }
+    };
     return error;
   }
 
   // credentials passed will be base_64 encoded
-  let credentials = new Buffer(auth.split(" ").pop(), "base64")
-  credentials = credentials.toString("ascii").split(":")
+  let credentials = new Buffer(auth.split(" ").pop(), "base64");
+  credentials = credentials.toString("ascii").split(":");
   let result = {
     auth: credentials
-
-  }
-  return result
+  };
+  return result;
 }
--- TestServer/index.js
+++ TestServer/index.js
@@ -1,45 +1,58 @@
-const path = require('path')
-const util = require('util')
-const express = require('express')
-const EventEmitter = require('events')
-const bodyParser = require('body-parser')
-const { authParser } = require('./helpers')
+const path = require('path');
+const util = require('util');
+const express = require('express');
+const EventEmitter = require('events');
+const bodyParser = require('body-parser');
+const { authParser } = require('./helpers');
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
   // useful for testing.
   response
   .status(200)
-  .sendFile(path.resolve('index.html'))
-})
+  .sendFile(path.resolve('demo/index.html'));
+});
+
+app.get('/main.css', (request, response) => {
+  // useful for testing.
+  response
+  .status(200)
+  .sendFile(path.resolve('demo/main.css'));
+});
+
+app.get('/demo.js', (request, response) => {
+  // useful for testing.
+  response
+  .status(200)
+  .sendFile(path.resolve('demo/demo.js'));
+});
 
 app.post('/test', (request, response) => {
-  const { auth } = authParser(request.get("authorization"), response)
-  response.send(`
-  <pre><code>
+  const { auth } = authParser(request.get("authorization"))
+  response.send(`<pre><code>
   Request parsed successfully
   -----------------------------------
+  Authorization: ${auth}
   Received data:
     ${util.format('%o', request.body)}
 
-  </code></pre>
-  `);
-})
+  </code></pre>`);
+});
 
 class Manager extends EventEmitter {}
-const manager = new Manager()
+const manager = new Manager();
 
 const port = +process.env.PORT || 9991
 var server = app.listen(port, () => {
-  console.log(`Testing Server started at ${port}`)
-  startTests()
-})
+  console.log(`Testing Server started at ${port}`);
+  startTests();
+});
 
 function startTests() {
-  manager.emit('server-started', server)
+  manager.emit('server-started', server);
 }
 
-module.exports = manager
+module.exports = manager;
