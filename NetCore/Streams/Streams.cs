328cb94f20fceee86c68ac644dd2c90a809c45f2
penCsharpener <matthiasmueller@protonmail.com>
2017-12-19 00:43:13 +0100

    Streams: added private GetJsonAsStringAsync()

--- NetCore/Streams/Streams.cs
+++ NetCore/Streams/Streams.cs
@@ -1,26 +1,35 @@
-
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ZulipNetCore {
 
-    public class Streams : EndPointBase {
+    public class Streams {
 
-        private static HttpClient _Client;
+        private static HttpClient _httpClient;
         private static ZulipClient _ZulipClient;
         public string JsonOutput;
         public StreamCollection StreamsCollection { get; set; }
 
-
         public Streams(ZulipClient ZulipClient) {
             _ZulipClient = ZulipClient;
-            _Client = ZulipClient.httpClient;
+            _httpClient = ZulipClient.httpClient;
         }
 
-        public async Task<string> GetStreams() {
-            JsonOutput  = await ConnectAsync(_Client, EndPointPath.Streams);
-            return JsonOutput;
+        public async Task<List<Stream>> GetStreamsAsync() {
+            await GetJsonAsStringAsync();
+            // continue with JsonOutput
+            return null;
         }
+
+        private async Task GetJsonAsStringAsync() {
+            // extra string variable not needed but useful for debugging
+            string TargetURL = $"{_ZulipClient.Server.ServerApiURL}/{EndPointPath.Streams}";
+            using (HttpResponseMessage Response = await _httpClient.GetAsync(TargetURL))
+            using (HttpContent content = Response.Content) {
+                JsonOutput = await content.ReadAsStringAsync();
+            }
+        }
+
     }
 }
