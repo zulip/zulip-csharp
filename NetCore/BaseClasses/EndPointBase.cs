44bb2b292aee2c018dc6bb31ab45315a0a8e2d5b
penCsharpener <matthiasmueller@protonmail.com>
2017-12-19 00:44:46 +0100

    EndPointBase: update

--- NetCore/BaseClasses/EndPointBase.cs
+++ NetCore/BaseClasses/EndPointBase.cs
@@ -5,20 +5,21 @@ namespace ZulipNetCore {
 
     public abstract class EndPointBase {
 
-        private static HttpClient _HttpClient;
-        public ZulipClient ZulipClient { get; set; }
+        public ZulipClient _ZulipClient { get; set; }
+        private HttpClient _HttpClient;
 
         public EndPointBase() {
 
         }
 
         public EndPointBase(ZulipClient ZulipClient) {
-            this.ZulipClient = ZulipClient;
+            this._ZulipClient = ZulipClient;
+            this._HttpClient = ZulipClient.httpClient;
         }
 
-        public async Task<string> ConnectAsync(HttpClient _HttpClient, string EndPointPath) {
+        public async Task<string> GetJsonString(string EndPointPath) {
             // extra string variable not needed but useful for debugging
-            string TargetURL = $"{ZulipClient.Server.BaseAddress.AbsoluteUri}/{EndPointPath}";
+            string TargetURL = $"{_ZulipClient.Server.BaseAddress.AbsoluteUri}/{EndPointPath}";
             using (HttpResponseMessage Response = await _HttpClient.GetAsync(TargetURL))
             using (HttpContent content = Response.Content) {
                 return await content.ReadAsStringAsync();
