2642de48d9cf711b15bb38bc7855cdd9da61152b
penCsharpener <matthiasmueller@protonmail.com>
2017-12-20 00:25:45 +0100

    Streams: can now return List<Stream>

--- NetCore/Streams/Streams.cs
+++ NetCore/Streams/Streams.cs
@@ -9,6 +9,8 @@ namespace ZulipNetCore {
         private static HttpClient _httpClient;
         private static ZulipClient _ZulipClient;
         public string JsonOutput;
+        public string ResponseMessage { get; private set; }
+        public string ResponseResult { get; private set; }
         public StreamCollection StreamsCollection { get; set; }
 
         public Streams(ZulipClient ZulipClient) {
@@ -18,11 +20,16 @@ namespace ZulipNetCore {
 
         public async Task<List<Stream>> GetStreamsAsync() {
             await GetJsonAsStringAsync();
-            // continue with JsonOutput
-            return null;
+            var Json = new JSONHelper();
+            dynamic JObj = Json.ParseJSON(JsonOutput);
+            ResponseMessage = JObj.msg;
+            ResponseResult = JObj.result;
+            object Streams = JObj.streams;
+
+            return Json.ParseJArray<Stream>(Streams);
         }
 
-        private async Task GetJsonAsStringAsync() {
+        public async Task GetJsonAsStringAsync() {
             // extra string variable not needed but useful for debugging
             string TargetURL = $"{_ZulipClient.Server.ServerApiURL}/{EndPointPath.Streams}";
             using (HttpResponseMessage Response = await _httpClient.GetAsync(TargetURL))
