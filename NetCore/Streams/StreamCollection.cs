f500d2917b6ab5c13b6cd5b27d369e13d57e4f20
penCsharpener <matthiasmueller@protonmail.com>
2017-12-20 00:25:07 +0100

    StreamCollection: add first basic draft

--- NetCore/Streams/StreamCollection.cs
+++ NetCore/Streams/StreamCollection.cs
@@ -5,7 +5,42 @@ using System.Text;
 
 namespace ZulipNetCore {
 
-    public class StreamCollection : CollectionBase {
+    public class StreamCollection : CollectionBase, IEnumerable, IEnumerator {
+
+        private int index = -1;
+
+        public StreamCollection() {
+            this.index = -1;
+        }
+
+        public void Add(Stream stream) {
+            if (stream != null) {
+                this.List.Add(stream);
+            }
+        }
+
+        public void Remove(Stream stream) {
+            this.List.Remove(stream);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return this;
+        }
+
+        public object Current {
+            get {
+                return this.List[index];
+            }
+        }
+
+        public bool MoveNext() {
+            this.index++;
+            return (this.index < this.List.Count);
+        }
+
+        public void Reset() {
+            this.index = -1;
+        }
 
     }
 }
