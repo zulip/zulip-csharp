dea0b3437c799f848ca38af1b90743c86bdfabf0
penCsharpener <matthiasmueller@protonmail.com>
2017-12-21 07:42:42 +0100

    User.cs: working

--- NetCore/Users/Users.cs
+++ NetCore/Users/Users.cs
@@ -2,16 +2,44 @@ using System.Net.Http;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ZulipNetCore {
 
-    public class Users : EndPointBase {
+    public class Users {
 
-        private static HttpClient _HttpClient;
-        private ZulipClient _ZulipClient;
+        private static HttpClient _httpClient;
+        private static ZulipClient _ZulipClient;
+        public string JsonOutput;
+        public string ResponseMessage { get; private set; }
+        public string ResponseResult { get; private set; }
+        private object ResponseArray;
+        //public UserCollection UsersCollection { get; set; }
 
         public Users(ZulipClient ZulipClient) {
             _ZulipClient = ZulipClient;
+            _httpClient = ZulipClient.Login();
+        }
+
+        public async Task<List<User>> GetUsersAsync() {
+            await GetJsonAsStringAsync();
+            var Json = new JSONHelper();
+
+            return Json.ParseJArray<User>(ResponseArray);
+        }
+
+        private async Task GetJsonAsStringAsync() {
+            // extra string variable not needed but useful for debugging
+            string TargetURL = $"{_ZulipClient.Server.ServerApiURL}/{EndPointPath.Users}";
+            using (HttpResponseMessage Response = await _httpClient.GetAsync(TargetURL))
+            using (HttpContent content = Response.Content) {
+                JsonOutput = string.Copy(await content.ReadAsStringAsync());
+            }
+            var Json = new JSONHelper();
+            dynamic JObj = Json.ParseJSON(JsonOutput);
+            ResponseMessage = JObj.msg;
+            ResponseResult = JObj.result;
+            ResponseArray = JObj.members;
         }
 
     }
