74ad82ecc5fad02dab4dd8d383803cda954eaa00
penCsharpener <matthiasmueller@protonmail.com>
2017-12-21 07:42:26 +0100

    add class User and BotTypes

---
+++ NetCore/Users/BotTypes.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZulipNetCore {
+
+    public enum BotTypes {
+        GenericBot =0,
+        InComingWebhook = 1,
+        OutGoingWebhook = 2
+    }
+}
