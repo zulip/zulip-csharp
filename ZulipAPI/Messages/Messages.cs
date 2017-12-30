b804779c5eb794fb9b6b40a77ffbed9b9f7e93ef
penCsharpener <matthiasmueller@protonmail.com>
2017-12-30 22:57:43 +0100

    Messages: get messages working :tada:

--- ZulipAPI/Messages/Messages.cs
+++ ZulipAPI/Messages/Messages.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ZulipAPI {
@@ -13,19 +15,55 @@ namespace ZulipAPI {
             _HttpClient = ZulipClient.Login();
         }
 
-        public async Task GetMessagesAsync() {
-            await GetJsonAsStringAsync(EndPointPath.Messages);
+        public async Task GetMessagesAsync(long Anchor = 9999999999999999,
+                                           long NumBefore = 9999999999999999,
+                                           long NumAfter = 1/*,
+                                           string Narrow = null,
+                                           bool UseFirstUnreadAnchor = false,
+                                           bool client_gravatar,
+                                           bool apply_markdown*/) {
+            var Parameters = new List<KeyValuePair<string, string>>() {
+                new KeyValuePair<string, string>("anchor", Anchor.ToString()),
+                new KeyValuePair<string, string>("num_before", NumBefore.ToString()),
+                new KeyValuePair<string, string>("num_after", NumAfter.ToString())/*,
+                new KeyValuePair<string, string>("narrow", Narrow),
+                new KeyValuePair<string, string>("use_first_unread_anchor", UseFirstUnreadAnchor.ToString()),
+                new KeyValuePair<string, string>("client_gravatar", ClientGravatar.ToString()),
+                new KeyValuePair<string, string>("apply_markdown", ApplyMarkdown.ToString()),*/
+            };
+
+            //https://github.com/zulip/zulip/blob/185fd99816eba15cb714b4c3a3f111cc40fde63f/zerver/views/messages.py#L558
+
+            await GetJsonAsStringAsync(Parameters);
+        }
+
+        protected async Task GetJsonAsStringAsync(List<KeyValuePair<string, string>> Parameters) {
+            string TargetURL = $"{_ZulipClient.Server.ServerApiURL}/{EndPointPath.Messages}";
+
+            using (HttpResponseMessage Response = await _HttpClient.GetAsync(TargetURL + Parameters.AsQueryString()))
+            using (HttpContent content = Response.Content) {
+                JsonOutput = string.Copy(await content.ReadAsStringAsync());
+            }
+            ParseResponse();
         }
 
         protected override void ParseResponse() {
             dynamic JObj = JSONHelper.ParseJSON(JsonOutput);
-            ResponseMessage = JObj.msg;
             ResponseResult = JObj.result;
+            ResponseMessage = JObj.msg;
             ResponseArray = JObj.messages;
 
+            List<object> lst = JSONHelper.ParseJArray<object>(ResponseArray);
+
             MessageCollection = new MessageCollection();
-            foreach (var msg in JSONHelper.ParseJArray<Message>(ResponseArray)) {
-                this.MessageCollection.Add(msg);
+            foreach (dynamic msg in lst) {
+                if (msg.type == "stream") {
+                    var sm = JSONHelper.ParseJObject<StreamMessage>(msg);
+                    MessageCollection.Add(sm);
+                } else if (msg.type == "private") {
+                    var pm = JSONHelper.ParseJObject<PrivateMessage>(msg);
+                    MessageCollection.Add(pm);
+                }
             }
         }
     }
