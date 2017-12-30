767f756ae50d91780beaffdf78039eb427e235d0
penCsharpener <matthiasmueller@protonmail.com>
2017-12-30 22:49:43 +0100

    MessageBase: renamed from Message

---
+++ ZulipAPI/Messages/MessageBase.cs
@@ -0,0 +1,50 @@
+using Newtonsoft.Json;
+
+namespace ZulipAPI {
+
+    public class MessageBase {
+
+        [JsonProperty("flags")]
+        public string[] Flags { get; set; }
+        [JsonProperty("client")]
+        public string Client { get; set; }
+        [JsonProperty("content")]
+        public string Content { get; set; }
+        [JsonProperty("sender_id")]
+        public int SenderID { get; set; }
+        [JsonProperty("sender_short_name")]
+        public string SenderShortName { get; set; }
+        [JsonProperty("avatar_url")]
+        public string AvartarURL { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("timestamp")]
+        public long TimeStamp { get; set; }
+        [JsonProperty("sender_realm_str")]
+        public string SenderRealmStr { get; set; }
+        [JsonProperty("recipient_id")]
+        public uint RecipientID { get; set; }
+        [JsonProperty("is_me_message")]
+        public bool IsMeMessage { get; set; }
+        [JsonProperty("sender_full_name")]
+        public string SenderFullName { get; set; }
+        //[JsonProperty("reactions")]
+        //public string[] Reactions { get; set; }
+        [JsonProperty("id")]
+        public ulong MessageID { get; set; }
+        [JsonProperty("subject_links")]
+        public string[] SubjectLinks { get; set; }
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+        [JsonProperty("sender_email")]
+        public string SenderEmail { get; set; }
+        [JsonProperty("content_type")]
+        public string ContentType { get; set; }
+
+        public override string ToString() {
+            return $"{MessageID} {SenderEmail}";
+        }
+
+    }
+}
