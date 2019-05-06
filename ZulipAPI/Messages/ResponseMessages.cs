3f601917d4c320fe00c051708da048fddde1ad6f
penCsharpener <matthiasmueller@protonmail.com>
2019-05-06 23:43:47 +0200

    remove obsolete files

--- ZulipAPI/Messages/ResponseMessages.cs
+++
@@ -1,9 +0,0 @@
-
-namespace ZulipAPI {
-    public class ResponseMessages : ResponseBase {
-
-        [Newtonsoft.Json.JsonProperty("messages")]
-        public object Messages { get; set; }
-
-    }
-}
--- ZulipAPI/Streams/ResponseStreams.cs
+++
@@ -1,8 +0,0 @@
-namespace ZulipAPI {
-    public class ResponseStreams : ResponseBase {
-
-        [Newtonsoft.Json.JsonProperty("streams")]
-        public object Streams { get; set; }
-
-    }
-}
